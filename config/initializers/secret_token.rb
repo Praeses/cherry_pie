2edecc570d6d37e6fb4c2e550810f1935dd5faeb
Lex Childs <lexchilds@gmail.com>
2013-01-25 08:33:11 -0600

    empty pie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CherryPie::Application.config.secret_token = '24619d18a64155dd8a01b03326d2e5f9dcc9f07d1172a9f378ba78dcb7689fa115a2a865ca24be92080197f8d5784d09cf8109aa2271063dedfc980dbc26c332'
