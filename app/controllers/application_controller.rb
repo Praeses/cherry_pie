d5150a8b1c247f19c54a6d29abc4ce8360f6bb1b
Lex Childs <lexchilds@gmail.com>
2013-01-25 09:06:19 -0600

    setup of current site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :setup_site
+
+  def setup_site
+    @current_site = Site.find_or_create_by_url( request.host )
+  end
+
 end
